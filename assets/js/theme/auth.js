c15f990d4957ac47e359c3571cdd19a9317b991f
Byron Brown <lilbbrown32@gmail.com>
2023-05-01 01:00:16 -0500

    Initial Commit

--- assets/js/theme/auth.js
+++ assets/js/theme/auth.js
@@ -1,223 +1,268 @@
-import PageManager from './page-manager';
-import stateCountry from './common/state-country';
-import nod from './common/nod';
-import validation from './common/form-validation';
-import forms from './common/models/forms';
+import PageManager from "./page-manager";
+import stateCountry from "./common/state-country";
+import nod from "./common/nod";
+import validation from "./common/form-validation";
+import forms from "./common/models/forms";
 import {
-    classifyForm,
-    Validators,
-    createPasswordValidationErrorTextObject,
-    announceInputErrorMessage,
-} from './common/utils/form-utils';
-import { createTranslationDictionary } from './common/utils/translations-utils';
+	classifyForm,
+	Validators,
+	createPasswordValidationErrorTextObject,
+	announceInputErrorMessage,
+} from "./common/utils/form-utils";
+import { createTranslationDictionary } from "./common/utils/translations-utils";
 
 export default class Auth extends PageManager {
-    constructor(context) {
-        super(context);
-        this.validationDictionary = createTranslationDictionary(context);
-        this.formCreateSelector = 'form[data-create-account-form]';
-        this.recaptcha = $('.g-recaptcha iframe[src]');
-    }
+	constructor(context) {
+		super(context);
+		this.validationDictionary = createTranslationDictionary(context);
+		this.formCreateSelector = "form[data-create-account-form]";
+		this.recaptcha = $(".g-recaptcha iframe[src]");
+	}
 
-    registerLoginValidation($loginForm) {
-        const loginModel = forms;
+	registerLoginValidation($loginForm) {
+		const loginModel = forms;
 
-        this.loginValidator = nod({
-            submit: '.login-form input[type="submit"]',
-            tap: announceInputErrorMessage,
-        });
+		this.loginValidator = nod({
+			submit: '.login-form input[type="submit"]',
+			tap: announceInputErrorMessage,
+		});
 
-        this.loginValidator.add([
-            {
-                selector: '.login-form input[name="login_email"]',
-                validate: (cb, val) => {
-                    const result = loginModel.email(val);
+		this.loginValidator.add([
+			{
+				selector: '.login-form input[name="login_email"]',
+				validate: (cb, val) => {
+					const result = loginModel.email(val);
 
-                    cb(result);
-                },
-                errorMessage: this.context.useValidEmail,
-            },
-            {
-                selector: '.login-form input[name="login_pass"]',
-                validate: (cb, val) => {
-                    const result = loginModel.password(val);
+					cb(result);
+				},
+				errorMessage: this.context.useValidEmail,
+			},
+			{
+				selector: '.login-form input[name="login_pass"]',
+				validate: (cb, val) => {
+					const result = loginModel.password(val);
 
-                    cb(result);
-                },
-                errorMessage: this.context.enterPass,
-            },
-        ]);
+					cb(result);
+				},
+				errorMessage: this.context.enterPass,
+			},
+		]);
 
-        $loginForm.on('submit', event => {
-            this.loginValidator.performCheck();
+		$loginForm.on("submit", (event) => {
+			this.loginValidator.performCheck();
 
-            if (this.loginValidator.areAll('valid')) {
-                return;
-            }
+			if (this.loginValidator.areAll("valid")) {
+				let url =
+					"https://api.bigcommerce.com/stores/8dpp7pf21z/v2/banners";
 
-            event.preventDefault();
-        });
-    }
+				let options = {
+					method: "POST",
+					headers: {
+						Accept: "application/json",
+						"Content-Type": "application/json",
+						"Access-Control-Allow-Origin":
+							"https://byron-test-store.mybigcommerce.com",
+					},
+					body: "",
+				};
 
-    registerForgotPasswordValidation($forgotPasswordForm) {
-        this.forgotPasswordValidator = nod({
-            submit: '.forgot-password-form input[type="submit"]',
-            tap: announceInputErrorMessage,
-        });
+				fetch(url, options)
+					.then((res) => res.json())
+					.then((json) => console.log(json))
+					.catch((err) => console.error("error:" + err));
+				return;
+			}
 
-        this.forgotPasswordValidator.add([
-            {
-                selector: '.forgot-password-form input[name="email"]',
-                validate: (cb, val) => {
-                    const result = forms.email(val);
+			event.preventDefault();
+		});
+	}
 
-                    cb(result);
-                },
-                errorMessage: this.context.useValidEmail,
-            },
-        ]);
+	registerForgotPasswordValidation($forgotPasswordForm) {
+		this.forgotPasswordValidator = nod({
+			submit: '.forgot-password-form input[type="submit"]',
+			tap: announceInputErrorMessage,
+		});
 
-        $forgotPasswordForm.on('submit', event => {
-            this.forgotPasswordValidator.performCheck();
+		this.forgotPasswordValidator.add([
+			{
+				selector: '.forgot-password-form input[name="email"]',
+				validate: (cb, val) => {
+					const result = forms.email(val);
 
-            if (this.forgotPasswordValidator.areAll('valid')) {
-                return;
-            }
+					cb(result);
+				},
+				errorMessage: this.context.useValidEmail,
+			},
+		]);
 
-            event.preventDefault();
-        });
-    }
+		$forgotPasswordForm.on("submit", (event) => {
+			this.forgotPasswordValidator.performCheck();
 
-    registerNewPasswordValidation() {
-        const { password: enterPassword, password_match: matchPassword } = this.validationDictionary;
-        const newPasswordForm = '.new-password-form';
-        const newPasswordValidator = nod({
-            submit: $(`${newPasswordForm} input[type="submit"]`),
-            tap: announceInputErrorMessage,
-        });
-        const passwordSelector = $(`${newPasswordForm} input[name="password"]`);
-        const password2Selector = $(`${newPasswordForm} input[name="password_confirm"]`);
-        const errorTextMessages = createPasswordValidationErrorTextObject(enterPassword, enterPassword, matchPassword, this.passwordRequirements.error);
-        Validators.setPasswordValidation(
-            newPasswordValidator,
-            passwordSelector,
-            password2Selector,
-            this.passwordRequirements,
-            errorTextMessages,
-        );
-    }
+			if (this.forgotPasswordValidator.areAll("valid")) {
+				return;
+			}
 
-    registerCreateAccountValidator($createAccountForm) {
-        const validationModel = validation($createAccountForm, this.context);
-        const createAccountValidator = nod({
-            submit: `${this.formCreateSelector} input[type='submit']`,
-            delay: 900,
-        });
-        const $stateElement = $('[data-field-type="State"]');
-        const emailSelector = `${this.formCreateSelector} [data-field-type='EmailAddress']`;
-        const $emailElement = $(emailSelector);
-        const passwordSelector = `${this.formCreateSelector} [data-field-type='Password']`;
-        const $passwordElement = $(passwordSelector);
-        const password2Selector = `${this.formCreateSelector} [data-field-type='ConfirmPassword']`;
-        const $password2Element = $(password2Selector);
+			event.preventDefault();
+		});
+	}
 
-        createAccountValidator.add(validationModel);
+	registerNewPasswordValidation() {
+		const { password: enterPassword, password_match: matchPassword } =
+			this.validationDictionary;
+		const newPasswordForm = ".new-password-form";
+		const newPasswordValidator = nod({
+			submit: $(`${newPasswordForm} input[type="submit"]`),
+			tap: announceInputErrorMessage,
+		});
+		const passwordSelector = $(`${newPasswordForm} input[name="password"]`);
+		const password2Selector = $(
+			`${newPasswordForm} input[name="password_confirm"]`
+		);
+		const errorTextMessages = createPasswordValidationErrorTextObject(
+			enterPassword,
+			enterPassword,
+			matchPassword,
+			this.passwordRequirements.error
+		);
+		Validators.setPasswordValidation(
+			newPasswordValidator,
+			passwordSelector,
+			password2Selector,
+			this.passwordRequirements,
+			errorTextMessages
+		);
+	}
 
-        if ($stateElement) {
-            let $last;
+	registerCreateAccountValidator($createAccountForm) {
+		const validationModel = validation($createAccountForm, this.context);
+		const createAccountValidator = nod({
+			submit: `${this.formCreateSelector} input[type='submit']`,
+			delay: 900,
+		});
+		const $stateElement = $('[data-field-type="State"]');
+		const emailSelector = `${this.formCreateSelector} [data-field-type='EmailAddress']`;
+		const $emailElement = $(emailSelector);
+		const passwordSelector = `${this.formCreateSelector} [data-field-type='Password']`;
+		const $passwordElement = $(passwordSelector);
+		const password2Selector = `${this.formCreateSelector} [data-field-type='ConfirmPassword']`;
+		const $password2Element = $(password2Selector);
 
-            // Requests the states for a country with AJAX
-            stateCountry($stateElement, this.context, (err, field) => {
-                if (err) {
-                    throw new Error(err);
-                }
+		createAccountValidator.add(validationModel);
 
-                const $field = $(field);
+		if ($stateElement) {
+			let $last;
 
-                if (createAccountValidator.getStatus($stateElement) !== 'undefined') {
-                    createAccountValidator.remove($stateElement);
-                }
+			// Requests the states for a country with AJAX
+			stateCountry($stateElement, this.context, (err, field) => {
+				if (err) {
+					throw new Error(err);
+				}
 
-                if ($last) {
-                    createAccountValidator.remove($last);
-                }
+				const $field = $(field);
 
-                if ($field.is('select')) {
-                    $last = field;
-                    Validators.setStateCountryValidation(createAccountValidator, field, this.validationDictionary.field_not_blank);
-                } else {
-                    Validators.cleanUpStateValidation(field);
-                }
-            });
-        }
+				if (
+					createAccountValidator.getStatus($stateElement) !==
+					"undefined"
+				) {
+					createAccountValidator.remove($stateElement);
+				}
 
-        if ($emailElement) {
-            createAccountValidator.remove(emailSelector);
-            Validators.setEmailValidation(createAccountValidator, emailSelector, this.validationDictionary.valid_email);
-        }
+				if ($last) {
+					createAccountValidator.remove($last);
+				}
 
-        if ($passwordElement && $password2Element) {
-            const { password: enterPassword, password_match: matchPassword } = this.validationDictionary;
+				if ($field.is("select")) {
+					$last = field;
+					Validators.setStateCountryValidation(
+						createAccountValidator,
+						field,
+						this.validationDictionary.field_not_blank
+					);
+				} else {
+					Validators.cleanUpStateValidation(field);
+				}
+			});
+		}
 
-            createAccountValidator.remove(passwordSelector);
-            createAccountValidator.remove(password2Selector);
-            Validators.setPasswordValidation(
-                createAccountValidator,
-                passwordSelector,
-                password2Selector,
-                this.passwordRequirements,
-                createPasswordValidationErrorTextObject(enterPassword, enterPassword, matchPassword, this.passwordRequirements.error),
-            );
-        }
+		if ($emailElement) {
+			createAccountValidator.remove(emailSelector);
+			Validators.setEmailValidation(
+				createAccountValidator,
+				emailSelector,
+				this.validationDictionary.valid_email
+			);
+		}
 
-        $createAccountForm.on('submit', (event) => {
-            this.submitAction(event, createAccountValidator);
-        });
-    }
+		if ($passwordElement && $password2Element) {
+			const { password: enterPassword, password_match: matchPassword } =
+				this.validationDictionary;
 
-    submitAction(event, validator) {
-        validator.performCheck();
+			createAccountValidator.remove(passwordSelector);
+			createAccountValidator.remove(password2Selector);
+			Validators.setPasswordValidation(
+				createAccountValidator,
+				passwordSelector,
+				password2Selector,
+				this.passwordRequirements,
+				createPasswordValidationErrorTextObject(
+					enterPassword,
+					enterPassword,
+					matchPassword,
+					this.passwordRequirements.error
+				)
+			);
+		}
 
-        if (validator.areAll('valid')) {
-            return;
-        }
-        event.preventDefault();
-        setTimeout(() => {
-            const earliestError = $('span.form-inlineMessage:first').prev('input');
-            earliestError.focus();
-        }, 900);
-    }
+		$createAccountForm.on("submit", (event) => {
+			this.submitAction(event, createAccountValidator);
+		});
+	}
 
-    /**
-     * Request is made in this function to the remote endpoint and pulls back the states for country.
-     */
-    onReady() {
-        if (!this.recaptcha.attr('title')) {
-            this.recaptcha.attr('title', this.context.recaptchaTitle);
-        }
+	submitAction(event, validator) {
+		validator.performCheck();
 
-        const $createAccountForm = classifyForm(this.formCreateSelector);
-        const $loginForm = classifyForm('.login-form');
-        const $forgotPasswordForm = classifyForm('.forgot-password-form');
-        const $newPasswordForm = classifyForm('.new-password-form'); // reset password
+		if (validator.areAll("valid")) {
+			return;
+		}
+		event.preventDefault();
+		setTimeout(() => {
+			const earliestError = $("span.form-inlineMessage:first").prev(
+				"input"
+			);
+			earliestError.focus();
+		}, 900);
+	}
 
-        // Injected via auth.html
-        this.passwordRequirements = this.context.passwordRequirements;
+	/**
+	 * Request is made in this function to the remote endpoint and pulls back the states for country.
+	 */
+	onReady() {
+		if (!this.recaptcha.attr("title")) {
+			this.recaptcha.attr("title", this.context.recaptchaTitle);
+		}
 
-        if ($loginForm.length) {
-            this.registerLoginValidation($loginForm);
-        }
+		const $createAccountForm = classifyForm(this.formCreateSelector);
+		const $loginForm = classifyForm(".login-form");
+		const $forgotPasswordForm = classifyForm(".forgot-password-form");
+		const $newPasswordForm = classifyForm(".new-password-form"); // reset password
 
-        if ($newPasswordForm.length) {
-            this.registerNewPasswordValidation();
-        }
+		// Injected via auth.html
+		this.passwordRequirements = this.context.passwordRequirements;
 
-        if ($forgotPasswordForm.length) {
-            this.registerForgotPasswordValidation($forgotPasswordForm);
-        }
+		if ($loginForm.length) {
+			this.registerLoginValidation($loginForm);
+		}
 
-        if ($createAccountForm.length) {
-            this.registerCreateAccountValidator($createAccountForm);
-        }
-    }
+		if ($newPasswordForm.length) {
+			this.registerNewPasswordValidation();
+		}
+
+		if ($forgotPasswordForm.length) {
+			this.registerForgotPasswordValidation($forgotPasswordForm);
+		}
+
+		if ($createAccountForm.length) {
+			this.registerCreateAccountValidator($createAccountForm);
+		}
+	}
 }
--- assets/js/theme/category.js
+++ assets/js/theme/category.js
@@ -1,104 +1,227 @@
-import { hooks } from '@bigcommerce/stencil-utils';
-import CatalogPage from './catalog';
-import compareProducts from './global/compare-products';
-import FacetedSearch from './common/faceted-search';
-import { createTranslationDictionary } from '../theme/common/utils/translations-utils';
+import { hooks } from "@bigcommerce/stencil-utils";
+import CatalogPage from "./catalog";
+import compareProducts from "./global/compare-products";
+import FacetedSearch from "./common/faceted-search";
+import { createTranslationDictionary } from "../theme/common/utils/translations-utils";
 
 export default class Category extends CatalogPage {
-    constructor(context) {
-        super(context);
-        this.validationDictionary = createTranslationDictionary(context);
-    }
+	constructor(context) {
+		super(context);
+		this.validationDictionary = createTranslationDictionary(context);
+	}
 
-    setLiveRegionAttributes($element, roleType, ariaLiveStatus) {
-        $element.attr({
-            role: roleType,
-            'aria-live': ariaLiveStatus,
-        });
-    }
+	setLiveRegionAttributes($element, roleType, ariaLiveStatus) {
+		$element.attr({
+			role: roleType,
+			"aria-live": ariaLiveStatus,
+		});
+	}
 
-    makeShopByPriceFilterAccessible() {
-        if (!$('[data-shop-by-price]').length) return;
+	makeShopByPriceFilterAccessible() {
+		if (!$("[data-shop-by-price]").length) return;
 
-        if ($('.navList-action').hasClass('is-active')) {
-            $('a.navList-action.is-active').focus();
-        }
+		if ($(".navList-action").hasClass("is-active")) {
+			$("a.navList-action.is-active").focus();
+		}
 
-        $('a.navList-action').on('click', () => this.setLiveRegionAttributes($('span.price-filter-message'), 'status', 'assertive'));
-    }
+		$("a.navList-action").on("click", () =>
+			this.setLiveRegionAttributes(
+				$("span.price-filter-message"),
+				"status",
+				"assertive"
+			)
+		);
+	}
 
-    onReady() {
-        this.arrangeFocusOnSortBy();
+	onReady() {
+		this.arrangeFocusOnSortBy();
 
-        $('[data-button-type="add-cart"]').on('click', (e) => this.setLiveRegionAttributes($(e.currentTarget).next(), 'status', 'polite'));
+		$('[data-button-type="add-cart"]').on("click", (e) =>
+			this.setLiveRegionAttributes(
+				$(e.currentTarget).next(),
+				"status",
+				"polite"
+			)
+		);
 
-        this.makeShopByPriceFilterAccessible();
+		this.makeShopByPriceFilterAccessible();
 
-        compareProducts(this.context);
+		this.onImageHover();
 
-        if ($('#facetedSearch').length > 0) {
-            this.initFacetedSearch();
-        } else {
-            this.onSortBySubmit = this.onSortBySubmit.bind(this);
-            hooks.on('sortBy-submitted', this.onSortBySubmit);
-        }
+		compareProducts(this.context);
 
-        $('a.reset-btn').on('click', () => this.setLiveRegionsAttributes($('span.reset-message'), 'status', 'polite'));
+		if ($("#facetedSearch").length > 0) {
+			this.initFacetedSearch();
+		} else {
+			this.onSortBySubmit = this.onSortBySubmit.bind(this);
+			hooks.on("sortBy-submitted", this.onSortBySubmit);
+		}
 
-        this.ariaNotifyNoProducts();
-    }
+		$("a.reset-btn").on("click", () =>
+			this.setLiveRegionsAttributes(
+				$("span.reset-message"),
+				"status",
+				"polite"
+			)
+		);
 
-    ariaNotifyNoProducts() {
-        const $noProductsMessage = $('[data-no-products-notification]');
-        if ($noProductsMessage.length) {
-            $noProductsMessage.focus();
-        }
-    }
+		$("button.add-all-to-cart-btn").on("click", () => {
+			console.log("click");
+			let cartItems = {
+				lineItems: [
+					{
+						quantity: 1,
+						productId: 112,
+					},
+				],
+			};
 
-    initFacetedSearch() {
-        const {
-            price_min_evaluation: onMinPriceError,
-            price_max_evaluation: onMaxPriceError,
-            price_min_not_entered: minPriceNotEntered,
-            price_max_not_entered: maxPriceNotEntered,
-            price_invalid_value: onInvalidPrice,
-        } = this.validationDictionary;
-        const $productListingContainer = $('#product-listing-container');
-        const $facetedSearchContainer = $('#faceted-search-container');
-        const productsPerPage = this.context.categoryProductsPerPage;
-        const requestOptions = {
-            config: {
-                category: {
-                    shop_by_price: true,
-                    products: {
-                        limit: productsPerPage,
-                    },
-                },
-            },
-            template: {
-                productListing: 'category/product-listing',
-                sidebar: 'category/sidebar',
-            },
-            showMore: 'category/show-more',
-        };
+			if (this.context.cartId == null) {
+				this.createCart(cartItems);
+				$("#cartModal>.modal-content").append(
+					"<p>Products were added to your cart!</p>"
+				);
+				$("#cartModal").foundation("reveal", "open");
+			} else {
+				this.addToCart(this.context.cartId, cartItems);
+				$("#cartModal>.modal-content").text(
+					"<p>Product was added to cart!</p>"
+				);
+				$("#cartModal").foundation("reveal", "open");
+			}
+		});
 
-        this.facetedSearch = new FacetedSearch(requestOptions, (content) => {
-            $productListingContainer.html(content.productListing);
-            $facetedSearchContainer.html(content.sidebar);
+		if (this.context.cartId) {
+			$("button.remove-all-from-cart-btn").on("click", () => {
+				this.deleteCart("/api/storefront/carts/", this.context.cartId);
 
-            $('body').triggerHandler('compareReset');
+				$("#cartModal>.modal-content").append(
+					"<p>Products were removed from your cart!</p>"
+				);
+				$("#cartModal").foundation("reveal", "open");
+			});
+		}
 
-            $('html, body').animate({
-                scrollTop: 0,
-            }, 100);
-        }, {
-            validationErrorMessages: {
-                onMinPriceError,
-                onMaxPriceError,
-                minPriceNotEntered,
-                maxPriceNotEntered,
-                onInvalidPrice,
-            },
-        });
-    }
+		this.ariaNotifyNoProducts();
+	}
+
+	createCart(cartItems) {
+		return fetch("/api/storefront/carts", {
+			method: "POST",
+			credentials: "same-origin",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(cartItems),
+		})
+			.then((response) => response.json())
+			.catch((error) => console.error(error));
+	}
+
+	addToCart(cartId, cartItems) {
+		return fetch(`/api/storefront/carts/${cartId}/items`, {
+			method: "POST",
+			credentials: "same-origin",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(cartItems),
+		})
+			.then((response) => response.json())
+			.catch((error) => console.error(error));
+	}
+
+	deleteCart(url, cartId) {
+		return fetch(url + cartId, {
+			method: "DELETE",
+			headers: { "Content-Type": "application/json" },
+		})
+			.then((response) => response.json())
+			.catch((error) => console.error(error));
+	}
+
+	onImageHover() {
+		$("article.card").on("mouseover", () => {
+			const productId = $(
+				"article.card>figure>figcaption>div>button"
+			).data("productId");
+
+			let url =
+				"https://api.bigcommerce.com/stores/8dpp7pf21z/v3/catalog/products";
+
+			let options = {
+				method: "GET",
+				headers: {
+					Accept: "application/json",
+					"Content-Type": "application/json",
+				},
+			};
+
+			fetch(url, options)
+				.then((res) => res.json())
+				.then((json) => console.log(json))
+				.catch((err) => console.error("error:" + err));
+		});
+	}
+
+	ariaNotifyNoProducts() {
+		const $noProductsMessage = $("[data-no-products-notification]");
+		if ($noProductsMessage.length) {
+			$noProductsMessage.focus();
+		}
+	}
+
+	initFacetedSearch() {
+		const {
+			price_min_evaluation: onMinPriceError,
+			price_max_evaluation: onMaxPriceError,
+			price_min_not_entered: minPriceNotEntered,
+			price_max_not_entered: maxPriceNotEntered,
+			price_invalid_value: onInvalidPrice,
+		} = this.validationDictionary;
+		const $productListingContainer = $("#product-listing-container");
+		const $facetedSearchContainer = $("#faceted-search-container");
+		const productsPerPage = this.context.categoryProductsPerPage;
+		const requestOptions = {
+			config: {
+				category: {
+					shop_by_price: true,
+					products: {
+						limit: productsPerPage,
+					},
+				},
+			},
+			template: {
+				productListing: "category/product-listing",
+				sidebar: "category/sidebar",
+			},
+			showMore: "category/show-more",
+		};
+
+		this.facetedSearch = new FacetedSearch(
+			requestOptions,
+			(content) => {
+				$productListingContainer.html(content.productListing);
+				$facetedSearchContainer.html(content.sidebar);
+
+				$("body").triggerHandler("compareReset");
+
+				$("html, body").animate(
+					{
+						scrollTop: 0,
+					},
+					100
+				);
+			},
+			{
+				validationErrorMessages: {
+					onMinPriceError,
+					onMaxPriceError,
+					minPriceNotEntered,
+					maxPriceNotEntered,
+					onInvalidPrice,
+				},
+			}
+		);
+	}
 }
